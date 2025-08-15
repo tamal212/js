78331753bbc196773c871a1bcc94136a9a94ac74
garu <tamalroy74658@gmail.com>
2025-08-15 12:38:15 +0600

    condition

---
+++ math.js
@@ -0,0 +1,14 @@
+
+
+let condition = 10;
+let condition2;
+switch (condition) {
+  case 13:
+    condition2 = 'it is a big number';
+    break;
+  case 10:
+    condition2 = 'it is a equal number';
+    break;
+  default:
+    con2 = 'not tobe undentiey';
+}
